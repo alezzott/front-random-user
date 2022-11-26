93b8b7cff85275b2a3c611f0d9a722b86b626c47
Alex Faustino <faustino.alex@outlook.com>
2022-11-25 21:50:04 -0300

    add style add rounded page number

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -19,7 +19,7 @@ const Pagination = (props: any) => {
     return <>
         {pageNumbers.map(Number => (
             <div key={Number} className='pag-div' >
-                <button onClick={() => handleClick(Number)} className={`p-8 py-[length:0.56rem] px-3 leading-tight cursor-pointer  border border-slate-600 text-white hover:bg-[#374151] 
+                <button onClick={() => handleClick(Number)} className={`p-8  rounded-md py-[length:0.56rem] px-3 leading-tight cursor-pointer  border border-slate-600 text-white hover:bg-[#374151] 
 
                 ${Number === activeNumber ? "bg-[#12212]" : "bg-[#1f2937]"}`}>
                     {Number}
