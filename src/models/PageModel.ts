bac3df2eddfef9ec73b56d742b8a80ce5d5844cb
Alex Faustino <faustino.alex@outlook.com>
2022-11-30 13:06:06 -0300

    chore: page model interface

--- src/models/PageModel.ts
+++ src/models/PageModel.ts
@@ -1,42 +1,13 @@
-export interface User {
-  gender: string;
-  name: Name;
-  location: Location;
-  email: string;
-  login: Login;
-  dob: Dob;
-  picture: Picture;
-  cell: string | undefined;
+import { SetStateAction } from "react";
+import { User } from "./UserModel";
+
+export interface PageProps {
+  totalPosts: any;
+  postsPerPage: any;
+  paginate: any;
 }
 
-export interface Name {
-  first: string;
-  last: string;
-}
-
-export interface Location {
-  street: Street;
-  city: string;
-  state: string;
-  country: string;
-}
-
-export interface Street {
-  number: number;
-  name: string;
-}
-
-export interface Login {
-  uuid: string;
-  username: string;
-  password: string;
-}
-
-export interface Dob {
-  date: string;
-  age: number;
-}
-
-export interface Picture {
-  large: string;
+export interface ModalProps {
+  setShowModal: React.Dispatch<SetStateAction<boolean>>;
+  currentUser: User | undefined;
 }
