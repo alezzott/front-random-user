dbe71f533654d29ed2d32209fa9d9f339705f9f8
Alex Faustino <faustino.alex@outlook.com>
2022-12-04 19:01:22 -0300

    update html semantic tags

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -14,28 +14,30 @@ export function Card({ currentPosts }: CardProps) {
   }
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 p-5">
+    <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 p-5">
       {currentPosts?.map((item: User, index: number) => (
-        <div
+        <article
           key={index}
           className=" bg-[#374151] w-full max-w-sm  border border-slate-600 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700"
         >
-          <div className="flex flex-col items-center pb-10">
-            <img
-              className="mt-4 w-24 h-24 mb-3 rounded-full shadow-lg"
-              src={item.picture.large}
-              alt={item.name.first}
-              title="Random User Title"
-            />
-            <h5 className="mb-1 text-xl font-medium">
+          <article className="flex flex-col items-center pb-10">
+            <figure id="user-image">
+              <img
+                className="mt-4 w-24 h-24 mb-3 rounded-full shadow-lg"
+                src={item.picture.large}
+                alt={item.name.first}
+                title="Random User Title"
+              />
+            </figure>
+            <h1 className="mb-1 text-xl font-medium">
               {item.name.first} {item.name.last}
-            </h5>
-            <span className="text-sm">{item.email}</span>
-            <span className="text-sm">{FormatPhone(item.cell)}</span>
-            <span className="text-sm">
+            </h1>
+            <p className="text-sm">{item.email}</p>
+            <p className="text-sm">{FormatPhone(item.cell)}</p>
+            <p className="text-sm">
               {item.location.city}, {item.location.country}
-            </span>
-            <div className="flex mt-4 space-x-3 md:mt-5">
+            </p>
+            <section className="flex mt-4 space-x-3 md:mt-5">
               <>
                 <button
                   className="block border-solid border-2 border-slate-600 text-white bg-[#374151] hover:bg-[#111827] focus:ring-4 focus:outline-non font-medium rounded-lg text-sm px-5 py-2.5 text-center"
@@ -53,10 +55,10 @@ export function Card({ currentPosts }: CardProps) {
                   />
                 ) : null}
               </>
-            </div>
-          </div>
-        </div>
+            </section>
+          </article>
+        </article>
       ))}
-    </div>
+    </section>
   );
 }
