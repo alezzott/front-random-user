0ea139721363270d4714502e4ef3c1c6b590763c
Alex Faustino <faustino.alex@outlook.com>
2022-12-15 23:30:00 -0300

    fix modal position

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -7,8 +7,8 @@ export function ModalComponent({ setShowModal, currentUser }: ModalProps) {
     <>
       <section
         onClick={() => setShowModal(false)}
-        className="z-[50] fixed backdrop-blur-sm backdrop-grayscale-0 backdrop-opacity-60 top-0 left-0 right-0  justify-center flex w-full p-4 overflow-x-hidden overflow-y-auto h-modal md:h-full">
-        <section className="z-[100] w-full h-full max-w-md md:h-auto">
+        className="z-[50] fixed backdrop-blur-sm backdrop-grayscale-0 backdrop-opacity-60 top-0 left-0 right-0 justify-center flex  p-4  overflow-y-auto  w-full overflow-x-hidden h-full md:h-full">
+        <section className="z-[100] md:h-auto mr-5 max-w-md  max-h-md h-full w-full ">
           <article
             className="border border-gray-700 bg-[#1f2937] rounded-lg shadow dark:bg-gray-700">
             <ModalTop currentUser={currentUser} setShowModal={setShowModal} closeDropdown={undefined} refIs={undefined} />
